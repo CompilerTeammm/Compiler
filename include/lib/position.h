6cdc63be1ff7d2b41eecfa12d9ae788304b4d969
zu-zi <141788731+zu-zi@users.noreply.github.com>
2025-01-22 22:05:00 +0800

    update position.h

--- include/lib/position.h
+++ include/lib/position.h
@@ -1,9 +1,14 @@
 #ifndef __POSITION_H__
 #define __POSITION_H__
-
-typedef struct position {
-  int line;
-  int col;
-} loc;
+#include <string>
+struct loc
+{
+  int line = 0;   // 行号
+  int column = 0; // 列号
+  std::string toString() const
+  {
+    return "Line: " + std::to_string(line) + ", Column: " + std::to_string(column);
+  }
+};
 
 #endif
