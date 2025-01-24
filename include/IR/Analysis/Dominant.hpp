d2cf40f8127b319669530b6e2aaa7186c595c24a
bigSheep123 <1554869970@qq.com>
2025-01-24 14:43:02 +0800

    try the dominantTree again

--- include/IR/Analysis/Dominant.hpp
+++ include/IR/Analysis/Dominant.hpp
@@ -1,29 +1,44 @@
 #include "../../lib/cfg/cfgg.hpp"
 #include <vector>
 // 搭建支配树
-class DominantTree
+class Lengauer_Tarjan
 {
 public:
+    struct Node
+    {
+        std::vector<Node*> pred;
+        std::vector<Node*> succ;
+
+        int dfs_num = 0;
+
+    };
+public:
     // 实现遍历的 dfs
-    void DFS(std::vector<BasicBlock*> cfg,std::vector<std::vector<int>> grap)
+    void DFS(std::vector<Node*> cfg,std::vector<std::vector<int>> grap)
     {
         int visited = 0;
-
-    }
+        // if(int i = 1; i <size+1; i++)
+            DFS();
+    };
     // sdom 初始化为自己
-    DominantTree()
+    Lengauer_Tarjan(int st)
     :sdom() {};
 
-    class Node
+    void DUS()
     {
 
     };
 
+    std::vector<Node*>& add_edge(int u, int v)
+    {
+        
+    }
+
 private:
-    std::vector<BasicBlock*> pred; // 
-    std::vector<BasicBlock*> parent;
-    std::vector<BasicBlock*> idom; // 直接支配节点数组 
-    std::vector<BasicBlock*> sdom;  // 半支配节点数组
+    int st;
+    std::vector<Node*> cfg;
+    std::vector<Node*> sdom;
+    std::vector<Node*> idom;
 };
 
 
