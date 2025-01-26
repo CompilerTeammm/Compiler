6863e5957d66e7c7304b76d3fb4fc1f59b10d9f9
bigSheep123 <1554869970@qq.com>
2025-01-26 19:36:12 +0800

    test dom_DFS

--- include/IR/Analysis/Dominant.hpp
+++ include/IR/Analysis/Dominant.hpp
@@ -1,42 +1,53 @@
-#include "../../lib/cfg/cfgg.hpp"
+// #include "../../lib/cfg/cfgg.hpp"
+#include <mutex>
 #include <vector>
+#include<iostream>
 // 搭建支配树
+// 步骤： 1. DFS , 得到DFS树和标号
+// 2. 逆序遍历 求 sdom
+// 3. 通过sdom 求得 idom
+// 4. 正序遍历一遍得到正确的 idom
+// 详情见论文
+
 class Lengauer_Tarjan
 {
+// 做测试，之后进行修改
+//    假设：
+// 没有建立图的关系，是在遍历节点之后建立图的关系
 public:
-    struct Node
+    struct Node 
     {
-        std::vector<Node*> pred;
+        // 用链表好，还是用数组呢？
+        std::vector<Node*> pre;
         std::vector<Node*> succ;
 
-        int dfs_num = 0;
-
-    };
-public:
-    // 实现遍历的 dfs
-    void DFS(std::vector<Node*> cfg,std::vector<std::vector<int>> grap)
-    {
-        int visited = 0;
-        // if(int i = 1; i <size+1; i++)
-            DFS();
-    };
-    // sdom 初始化为自己
-    Lengauer_Tarjan(int st)
-    :sdom() {};
-
-    void DUS()
-    {
-
+        int flag = 0;
+        static int order_num;
+    
+        bool isVisited()
+        {
+            return flag != 0;
+        }
     };
 
-    std::vector<Node*>& add_edge(int u, int v)
-    {
-        
+    Lengauer_Tarjan(int _n,int _m)
+        :node_num(_n),
+        edge_num(_m)
+    { 
+        CFG =std::vector<Node> (node_num+1);
     }
 
-private:
-    int st;
-    std::vector<Node*> cfg;
+    void add_edge(int u, int v);
+    void dump();
+    void Run();
+    void DFS_func();
+
+    int node_num;
+    int edge_num;
+    int dfs_cnt = 0;
+    std::vector<Node> CFG;
+    std::vector<Node*> DFS_order;
+
     std::vector<Node*> sdom;
     std::vector<Node*> idom;
 };
