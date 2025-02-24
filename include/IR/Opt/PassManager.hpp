13ec311d3dfb63bb7acfc6fe5c537b5bbb8524dc
bigSheep123 <1554869970@qq.com>
2025-02-24 14:22:24 +0800

    runimpl try

--- include/IR/Opt/PassManager.hpp
+++ include/IR/Opt/PassManager.hpp
@@ -1,10 +1,14 @@
 #pragma once
 #include"Passbase.hpp"
 #include "CoreClass.hpp"
-
+#include <memory>
+#include <type_traits>
+// using namespace std;
 // 用来管理Passes
-class FunctionPassManager;
-class MoudlePassManager;
+class Function;
+class Module;
+template <> class _PassManagerBase<class Pass, class myType>;
+
 enum PassName {
     mem2reg,
 
@@ -13,6 +17,26 @@ enum PassName {
 class _AnalysisManager:
     public _AnalysisManagerBase<_AnalysisManager,Function>
 {
-    void InitPass(_AnalysisManager pass)
-    { }
+   
+};
+
+class _PassManager:
+    public _PassManagerBase<_PassManager,Function>
+{
+    bool Run();
+
+public:
+    // 检查条件为 true ： name的类型为void 模板正常实例化
+    template<typename Pass, typename name = std::enable_if_t<std::is_base_of_v<_PassManagerBase<Pass,Module>,Pass>>>
+    bool RunImpl(Module* mod,_AnalysisManager &AM){
+        auto pass = std::make_unique<Pass>(mod,AM);
+        return pass->Run();
+    }
+
+    template<typename Pass,typename name = std::enable_if_t<std::is_base_of_v<_PassManagerBase<Pass,Function>,Pass>>>
+    bool RunImpl(Function* func,_AnalysisManager &AM)
+    {
+        auto pass = std::make_unique<Pass>(func,AM);
+        return pass->Run();
+    }
 };
--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
 ///// for test
+#include "PassManager.hpp"
+#include <memory>
+
+
+int main()
+{
+    auto PM = std::make_unique<_PassManager>();
+    PM->RunImpl();
+}
