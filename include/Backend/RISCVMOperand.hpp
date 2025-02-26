5fb5a6bf056dfdd1db292b4f9c28b59f17ca638c
hu1 <3047853373@qq.com>
2025-02-26 22:26:06 +0800

    - `backend/graphdraw.cpp`：新增文件，实现 [具体功能描述]。 - `include/Backend/RISCVMOperand.hpp`：新增文件，定义 RISC-V 操作数相关的内容。 - `include/Backend/RISCVRegister.hpp`：新增文件，定义 RISC-V 寄存器相关的内容。

---
+++ include/Backend/RISCVMOperand.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include "../../include/backend/RISCVType.hpp"
+#include "../../include/lib/BaseCFG.hpp"
+
+class Register;
+
+class RISCVMOperand {
+    RISCVType tp;
+    public:
+    RISCVMOperand(RISCVType _tp) : tp(_tp) {};
+    RISCVType GetType(){
+        return tp;}
+    virtual void print()=0;
+    
+    template<typename T>
+    T* as(){
+        return dynamic_cast<T*>(this);
+    }
+    Register* ignoreLA();
+};
+
+//立即数封装类
+class Imm : public RISCVMOperand {
+    ConstantData* data;
+    public:
+    Imm(ConstantData* );
+    ConstantData* GetData();
+    static Imm* GetImm(ConstantData*);
+    void print()final;
+}
