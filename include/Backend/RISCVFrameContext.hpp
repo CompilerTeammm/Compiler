efce249886037b15dd5fb03fd0318fedfc634112
hu1 <3047853373@qq.com>
2025-03-12 19:53:53 +0800

    test

--- include/Backend/RISCVFrameContext.hpp
+++ include/Backend/RISCVFrameContext.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "../../include/Backend/RISCVMOperand.hpp"
 #include "../../include/Backend/RISCVRegister.hpp"
-//#include "../../include/lib/MagicEnum.hpp"
+#include "../../include/lib/MagicEnum.hpp"
 //#include "../../include/lib/MagicEnum.hpp"
 //to do：待我研究一下这个库是个啥
 //有名字的操作数
