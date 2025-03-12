38eb1ffcf2d6f6ccf2f519bf9fdf5c78a316e7ac
hu1 <3047853373@qq.com>
2025-03-12 21:52:54 +0800

    begin RISCVAsmPrinter.hpp

--- include/Backend/RISCVAsmPrinter.hpp
+++ include/Backend/RISCVAsmPrinter.hpp
@@ -2,11 +2,11 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
-// #include "../../include/Backend/RISCVFrameContext.hpp"
-// #include "../../include/Backend/RISCVMIR.hpp"
-// #include "../../include/Backend/RISCVContext.hpp"
-// #include "../../include/Backend/FloatToDex.hpp"
-// #include "../../include/lib/MagicEnum.hpp"
+#include "../../include/Backend/RISCVFrameContext.hpp"
+#include "../../include/Backend/RISCVMIR.hpp"
+#include "../../include/Backend/RISCVContext.hpp"
+#include "../../include/Backend/FloatToDex.hpp"
+#include "../../include/lib/MagicEnum.hpp"
 
 class globlvar;
 class tempvar;
@@ -34,19 +34,28 @@ public:
     void printAsmGlobal();
     void printAsm();
 
+    void set_use_cachelookup(bool);//缓存查找优化
+    void set_use_cachelookup4(bool);
+    void printCacheLookUp();
+    void printCacheLookUp4();
+//    void printParallelLib();//输出并行计算相关汇编代码
+
 protected:
     std::string filename; // 输出文件名
     dataSegment *data;    // 数据段
     textSegment *text;    // 代码段
+    bool use_cachelookup =false;
+    bool use_cachelookup4 =false;
+    std::string cachefilepath="RISCVLib/Cachelib.s"//缓存库路径
 };
 
 class dataSegment
 {
 public:
-    dataSegment(Moudle *moudle, RISCVLoweringContext &ctx);
+    dataSegment(Moudle *moudle, RISCVLoweringContext &ctx);//通过 Module* 获取所有全局变量，并存入 globlvar_list。
     ~dataSegment() = default;
-    void GenerateGloblvarList(Module *module, RISCVLoweringContext &ctx);
-    void GenerateTempvarList(RISCVLoweringContext &ctx);
+    void GenerateGloblvarList(Module *module, RISCVLoweringContext &ctx);//遍历 Module* 获取所有全局变量。
+    void GenerateTempvarList(RISCVLoweringContext &ctx);//收集 RISCVMIR 中浮点数常量，转换成 tempvar 存入 .data 段。
     std::vector<tempvar *> get_tempvar_list();
     void Change_LoadConstFloat(RISCVMIR *inst, tempvar *tempfloat, List<RISCVBasicBlock, RISCVMIR>::iterator it, Imm *used);
     void PrintDataSegment_Globval();
@@ -54,9 +63,9 @@ public:
     void LegalizeGloablVar(RISCVLoweringContext &);
 
 private:
-    std::vector<globlvar *> globlvar_list;
-    int num_lable;
-    std::vector<tempvar *> tempvar_list;
+    std::vector<globlvar *> globlvar_list;//.data存储全局变量
+    int num_lable;//变量计数
+    std::vector<tempvar *> tempvar_list;//.data存储浮点数临时变量
 };
 
 class textSegment
