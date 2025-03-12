b59480585557f5b4a2a64288c6347723bb9ed30f
hu1 <3047853373@qq.com>
2025-03-12 22:30:22 +0800

    RISCVAsmPrinter.hpp

--- include/Backend/RISCVAsmPrinter.hpp
+++ include/Backend/RISCVAsmPrinter.hpp
@@ -58,15 +58,42 @@ public:
     void GenerateTempvarList(RISCVLoweringContext &ctx);//收集 RISCVMIR 中浮点数常量，转换成 tempvar 存入 .data 段。
     std::vector<tempvar *> get_tempvar_list();
     void Change_LoadConstFloat(RISCVMIR *inst, tempvar *tempfloat, List<RISCVBasicBlock, RISCVMIR>::iterator it, Imm *used);
+    //修改 RISCVMIR 指令，使浮点数存入 tempvar 并存放在 .data 段。
     void PrintDataSegment_Globval();
     void PrintDataSegment_Tempvar();
-    void LegalizeGloablVar(RISCVLoweringContext &);
+    void LegalizeGloablVar(RISCVLoweringContext &);//
 
 private:
     std::vector<globlvar *> globlvar_list;//.data存储全局变量
-    int num_lable;//变量计数
-    std::vector<tempvar *> tempvar_list;//.data存储浮点数临时变量
+    int num_lable;//变量计数，用于生成唯一标签，给 tempvar 使用，以防止变量名重复。
+    std::vector<tempvar *> tempvar_list;//存储浮点数常量
 };
+class globlvar:public RISCVGlobalObject{
+    private:
+    int align;
+    std::string ty="object";
+    int size;
+    std::vector<std::variant<int,float>> init_vector;
+    public:
+    globlvar(Variable* data);
+    ~globlvar()=default;
+    void generate_array_init(Initializer* arry_init,Type* basetype);
+    void PrintGloblvar();
+}
+class tempvar:public RISCVTempFloatObject{
+    private:
+    int num_lable;
+    int align;
+    float init;
+    public:
+    tempvar(int num_lable,float init);
+    ~tempvar()=default;
+    std::string Getname();
+    float GetInit(){
+        return init;
+    }
+    void PrintTempvar();
+}
 
 class textSegment
 {
@@ -77,6 +104,7 @@ public:
     textSegment(RISCVLoweringContext &ctx);
     void GenerateFuncList(RISCVLoweringContext &ctx);
     void PrintTextSwgment();
-} class functionSegment
+} 
+class functionSegment
 {
 }
