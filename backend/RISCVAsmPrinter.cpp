8c62462f3385cd381b42e11e86014eda3e436efe
hu1 <3047853373@qq.com>
2025-03-13 21:45:15 +0800

    begin update RISCVAsmPrinter.cpp

--- backend/RISCVAsmPrinter.cpp
+++ backend/RISCVAsmPrinter.cpp
@@ -2,12 +2,29 @@
 
 SegmentType __oldtype =TEXT;
 SegmentType* oldtype =&__oldtype;
-
+//真change了吗?
 SegmentType ChangeSegmentType(SegmentType newtype){
     return newtype;
 }
 
-
+void PrintSegmentType(SegmentType newtype,SegmentType* oldtype){
+    if(newtype=*oldtype){
+        return;
+    }else{
+        *oldtype=ChangeSegmentType(newtype);
+        if(newtype==TEXT){
+            std::cout<<"    .text"<<std::endl;
+        }else if(newtype==DATA){
+            std::cout<<"    .data"<<std::endl;
+        }else if(newtype==BSS){
+            std::cout<<"    .bss"<<std::endl;
+        }else if(newtype==RODATA){
+            std::cout<<"    .section    .rodata"<<std::endl;
+        }else{
+            std::cout<<"ERROR: Illegal SegmentType"<<std::endl;
+        }
+    }
+}
 //AsmPrinter 初始化一个实例 
 //LLVM 的 Module 类代表了一个编译单元（Compilation Unit）
 RISCVAsmPrinter::RISCVAsmPrinter(std::string filename,Moudle* unit, RISCVLoweringContext& ctx): filename(filename){
