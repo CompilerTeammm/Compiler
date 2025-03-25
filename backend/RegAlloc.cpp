c817f2d0ba815aff9d116c75f91ed408d28f7f57
hu1 <3047853373@qq.com>
2025-03-25 21:26:38 +0800

    begin Liveness,RegAlloc,LegalizePass

---
+++ backend/RegAlloc.cpp
@@ -0,0 +1,1 @@
+#include "../include/Backend/RegAlloc.hpp"
--- include/Backend/Liveness.hpp
+++ include/Backend/Liveness.hpp
@@ -1,16 +1,26 @@
-#include "../../include/Backend/RISCVMIR.hpp"
+#pragma once
+#include "../../include/backend/RISCVContext.hpp"
+#include "../../include/backend/RISCVMIR.hpp"
+#include "../../include/backend/RISCVMOperand.hpp"
+#include "../../include/backend/RISCVRegister.hpp"
+#include "../../include/backend/RISCVType.hpp"
+#include "../../include/lib/CFG.hpp"
+#include "../../util/my_stl.hpp"
+#include <unordered_map>
+#include <unordered_set>
 
-using BlockInfo = Liveness;
+using MOperand =Register*;
 
-class Liveness
-{
-public:
-  Liveness(RISCVFunction *f)
-  {
-  }
+class Liveness{
+  private:
+  void GetBlockLivein(RISCVBasicBlock *block);
+  void GetBlockLiveout(RISCVBasicBlock *block);
+  RISCVFunction *m_func;
+
+  public:
   void CalCulateSucc(RISCVBasicBlock *block);
+  std::unordered_map<RISCVBasicBlock*,std::list<RISCVBasicBlock*>> SuccBlocks;
+  std::unordered_map<RISCVBasicBlock*,std::unordered_set<MOperand>> BlockLivein;
+  std::unordered_map<RISCVBasicBlock*,std::unordered_set<MOperand>> BlockLiveout;
+  
 };
-
-void BlockInfo::CalCulateSucc(RISCVBasicBlock *block)
-{
-}
