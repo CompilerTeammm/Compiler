0e31ffa07cdc0dc520e8d35cafad6979d6ef6690
hu1 <3047853373@qq.com>
2025-03-27 23:13:51 +0800

    livein liveout

--- backend/Liveness.cpp
+++ backend/Liveness.cpp
@@ -12,6 +12,23 @@ using InterVal = LiveInterval;
 using OpType = RISCVMIR::RISCVISA;
 void BlockInfo::RunOnFunction(){
     for(RISCVBasicBlock *block:*m_func){
-        
+        BlockLivein[block].clear();
+        BlockLiveout[block].clear();
+        GetBlockLivein(block);
+        GetBlockLiveout(block);
+    }
+    bool modified=true;//是否需要迭代
+    while(modified){
+        modified=false;
+        //逆序遍历计算livein liveout
+        for(auto block=m_func->rbegin();block!=m_func->rend();--block){
+            RISCVBasicBlock *cur = *block;
+            std::unordered_set<MOperand> oldin = BlockLivein[cur];//记录当前livein
+            GetBlockLiveout(cur);//计算当前基本块liveout
+            BlockLivein[cur] = BlockLiveout[cur];//先将out赋值给in
+            GetBlockLivein(cur);//计算livein
+            if (BlockLivein[cur] != oldin)
+                modified = true;
+        }
     }
 }
