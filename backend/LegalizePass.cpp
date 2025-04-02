1cc88faee34c5eb0d698d312806d5429efb2244d
hu1 <3047853373@qq.com>
2025-04-02 15:45:49 +0800

    转换伪指令为RISCV指令：global

--- backend/LegalizePass.cpp
+++ backend/LegalizePass.cpp
@@ -5,7 +5,7 @@
 Legalize::Legalize(RISCVLoweringContext& _ctx):ctx(_ctx){}
 
 void Legalize::run(){
-    // fold 2 stackreg    
+    // 将load/store指令中的物理寄存器转化为栈寄存器   
     { 
         auto func=ctx.GetCurFunction();
         for(auto block:*func){
@@ -25,7 +25,49 @@ void Legalize::run(){
             }
         }
     }
+    //将某些伪指令转化为标准的RISCV指令
     {
-        
+        auto func=ctx.GetCurFunction();
+        for(auto block:*func){
+            for(auto it=block->begin();it!=block->end();){
+                auto inst=*it;
+                auto opcode=inst->GetOpcode();
+                switch(opcode){
+                    case RISCV::LoadGlobalAddr:{
+                        //lambda 表达式:是一种匿名函数，可以作为变量存储并在后续调用。
+                        //使代码更简洁
+                        // reg = LoadGlobalAddr globalvar
+                        // lui reg, %hi(globalvar)
+                        // addi reg, reg, %lo(globalvar)
+                        auto getTagName=[&](){
+                            if(auto glob=inst->GetOperand(0)->as<globlvar>()){
+                                return glob->GetName();
+                            }
+                            else if(auto tag=inst->GetOperand(0)->as<OuterTag>()){
+                                return tag->GetName();
+                            }
+                            else assert(0&&"GG in get global address");
+                        };
+                        // 也可以类似：
+                        // std::string getTagName(RISCVMIR* inst) {
+                        //     if (auto glob = inst->GetOperand(0)->as<globlvar>()) {
+                        //         return glob->GetName();
+                        //     }
+                        //     else if (auto tag = inst->GetOperand(0)->as<OuterTag>()) {
+                        //         return tag->GetName();
+                        //     }
+                        //     else {
+                        //         assert(0 && "GG in get global address");
+                        //         return "";
+                        //     }
+                        // }
+                        auto name=getTagName();
+                        auto hi=new LARegister(riscv_ptr,name,LARegister::hi);
+                        auto lo=new LARegister(riscv_ptr,name,LARegister::lo);
+                        
+                    }
+                }
+            }
+        }
     }
 }
