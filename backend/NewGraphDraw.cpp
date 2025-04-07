f73b4b0bf59f68111c58348f2ee2e0b5056dc31f
hu1 <3047853373@qq.com>
2025-04-07 21:22:32 +0800

    都怪大国点名,剩下的明天写完 part of NewGraphDraw.cpp

--- backend/NewGraphDraw.cpp
+++ backend/NewGraphDraw.cpp
@@ -12,7 +12,7 @@
 #include <iostream>
 #include <ostream>
 #include <unordered_set>
-
+#include <unordered_map>
 //入口
 void GraphColor::RunOnFunc(){
     bool condition=true;
@@ -106,7 +106,7 @@ MOperand GraphColor::GetAlias(MOperand v){
 // Briggs：评估合并后整体邻居中度数过高的数量是否可控。
 bool GraphColor::GeorgeCheck(MOperand dst,MOperand src,RISCVType type){
   //George 启发式适用于 src 是普通节点、dst 是预着色节点。
-  if(type==riscv_i32||type=riscv_ptr||type=riscv_i64){
+  if(type==riscv_i32||type==riscv_ptr||type==riscv_i64){
     auto x=Adjacent(src);//获取src的邻接节点
     for(auto tmp:x){
       // bool ok=false;
@@ -138,6 +138,328 @@ bool GraphColor::GeorgeCheck(MOperand dst,MOperand src,RISCVType type){
       }
     }
   }else if(type==riscv_float32){
-
+    auto x=Adjacent(src);
+    for(auto tmp:x){
+      // bool ok = false;
+      // if (Degree[tmp] < reglist.GetReglistFloat().size())
+      //   ok |= true;
+      // if (Precolored.find(tmp) != Precolored.end())
+      //   ok |= true;
+      // auto &adj = adjSet[dst];
+      // if (adj.find(tmp) != adj.end())
+      //   ok |= true;
+      // if (ok != true)
+      //   return false;
+      //等价简洁版:
+      auto &adj = adjSet[dst];
+      if (!(Degree[tmp] < reglist.GetReglistFloat().size() ||
+            Precolored.find(tmp) != Precolored.end() ||
+            adj.find(tmp) != adj.end())){
+          return false;
+      }
+    }
+  }else{
+    assert(0 && "type must be either int or float");
   }
+  return true;
+}
+
+bool GraphColor::BriggsCheck(MOperand dst,MOperand src,RISCVType type){
+  auto tmp=Adjacent(dst);
+  std::unordered_set<MOperand> target{tmp.begin(), tmp.end()};
+  tmp = Adjacent(src);
+  target.insert(tmp.begin(), tmp.end());//此时 target 包含了 dst 和 src 的合并后邻接集合。
+  if(type==riscv_i32||type==riscv_ptr||type==riscv_i64){
+    int num=0;
+    for(auto node : target){
+      if(Degree[node]>=reglist.GetReglistInt().size()){
+        num++;//记录邻接集合中“冲突严重”的节点数（即度数大于等于寄存器数量的点）。
+      }
+    }
+    return (num<reglist.GetReglistInt().size());
+    //如果合并后冲突严重的邻接节点数少于寄存器数 ⇒ 可以安全合并；
+    //反之，合并可能导致溢出（spill），不建议合并。
+  }else if(type==riscv_float32){
+    int num = 0;
+    for (auto node : target) {
+      if (Degree[node] >= reglist.GetReglistFloat().size()){
+        num++;
+      }
+    }
+    return (num < reglist.GetReglistFloat().size());
+  }else{
+    return true;
+  }
+}
+
+//将一个非预着色（非物理寄存器）节点 v 加入 simplifyWorkList
+void GraphColor::AddWorkList(MOperand v){
+  if (Precolored.find(v) == Precolored.end() && Degree[v] < GetRegNums(v) && MoveRelated(v).size() == 0) {
+    //如果v不是预着色寄存器(也就不是物理寄存器),也不是高阶节点(度数小于可用寄存器数)且不参与任何move指令(不是合并候选者)
+    freezeWorkList.erase(v);
+    PushVecSingleVal(simplifyWorkList, v);
+    //那就移除,说明不需要冻结,压入s后准备进行简化
+  }
+}
+//用于计算活跃信息的主函数
+void GraphColor::CaculateLiveness() {
+  LiveInfoInit();//初始化活跃性信息数据结构
+  RunOnFunction();//函数级别进行活跃性分析
+  //计算IG,并且添加precolored集合
+  Build();
+  RunOnFunc_();//运行后处理
+  for (const auto b : *m_func) {
+    CaculateLiveInterval(b);//遍历基本块,建立虚拟寄存器的活跃区间信息
+  }
+}
+
+//计算活跃区间并合并成全局结果
+void GraphColor::CaculateLiveInterval(RISCVBasicBlock *bb){
+  auto &IntervInfo=GetRegLiveInterval(bb);
+  for(auto &[val, vec] : IntervInfo){
+    //遍历所有寄存器val和其在当前块的活跃区间vec
+    if (!GlobalLiveRange.count(val)) {
+      GlobalLiveRange[val].start = INT32_MAX;
+      GlobalLiveRange[val].end = INT32_MIN;
+    }//如果这个寄存器还没有全局活跃区间记录,则初始化
+    unsigned int length = 0;
+    for (auto v : vec) {
+      if (v.start < GlobalLiveRange[val].start){
+        GlobalLiveRange[val].start = v.start;
+      }
+      if (v.end > GlobalLiveRange[val].end){
+        GlobalLiveRange[val].end = v.end;
+      }
+      length += v.end - v.start;
+    }
+    ValsInterval[val] = length;
+  }
+}
+//在 spillWorkList 中选择一个“最适合溢出”的虚拟寄存器，根据多个启发式因素评分：活跃长度、度数、惩罚值。
+MOperand GraphColor::HeuristicSpill(){
+  float max=0;//记录当前最大权重
+  Register *sp=nullptr;
+  for(auto spill:spillWorkList){
+    auto vspill = dynamic_cast<VirRegister *>(spill);
+    float weight=0;//初始化当前节点的综合权重
+
+    //degree
+    int degree=Degree[spill];
+    if(degree<0){
+      assert(0);
+    }
+    weight=weight+(degree*DegreeWeight);
+    //interval
+    int intervalLength=ValsInterval[spill];
+    weight=weight+(intervalLength * livenessWeight);
+    //考虑 penalty 惩罚：spill 和 reload 的开销
+    weight=weight+(vspill->GetPenaltySpill() + vspill->GetPenaltyReload()) * SpillWeight;
+    //比较替换
+    if (max < weight) {
+      max = weight;
+      sp = spill;
+      continue;
+    }
+  }
+  if (!sp)
+  for (auto spill : spillWorkList){
+    return spill;
+  }
+  return sp;
+}
+
+//启发式选择一个可以冻结的寄存器，目前实现是：直接选第一个节点
+MOperand GraphColor::HeuristicFreeze() {
+  return *(freezeWorkList.begin()); 
+}
+void GraphColor::SetRegState(PhyRegister *reg, RISCVType ty) {
+  RegType[reg] = ty;//标记物理寄存器类型,在获取类型相关寄存器数量时会用到
+}
+int GraphColor::GetRegNums(MOperand v) {
+  if (v->GetType() == riscv_i32 || v->GetType() == riscv_ptr || v->GetType() == riscv_i64)
+    return reglist.GetReglistInt().size();
+  else if (v->GetType() == riscv_float32)
+    return reglist.GetReglistFloat().size();
+  else if (v->GetType() == riscv_none) {
+    auto preg = dynamic_cast<PhyRegister *>(v);
+    auto tp = RegType[preg];
+    assert(tp != RISCVType::riscv_none && "error");
+    return (tp == riscv_i32 || tp == riscv_i64) ? reglist.GetReglistInt().size()
+                           : reglist.GetReglistFloat().size();
+  }
+  assert("excetion case");
+  return 0;
+}
+int GraphColor::GetRegNums(RISCVType ty) {
+  if (ty == riscv_i32 || ty == riscv_ptr || ty == riscv_i64)
+    return reglist.GetReglistInt().size();
+  if (ty == riscv_float32)
+    return reglist.GetReglistFloat().size();
+  assert(0);
+}
+
+//节点合并
+void GraphColor::combine(MOperand rd,MOperand rs){
+  //合并时确保类型一致
+  if(rd->GetType() == riscv_none && rs->GetType() != riscv_none){
+    RegType[dynamic_cast<PhyRegister *>(rd)] = rs->GetType();
+  }else if(rd->GetType() != riscv_none && rs->GetType() == riscv_none){
+    RegType[dynamic_cast<PhyRegister *>(rs)] = rd->GetType();
+  }
+  //合并后，rs 不再是活跃候选节点，所以从 freeze 或 spill 工作列表中移除。
+  if (freezeWorkList.find(rs) != freezeWorkList.end()){
+    freezeWorkList.erase(rs);
+  }else{
+    spillWorkList.erase(rs);
+  }
+  coalescedNodes.insert(rs);
+  alias[rs] = rd;
+  //更新合并后的movelist,干涉图
+  for (auto mv : moveList[rs]) {
+    moveList[rd].insert(mv);
+  }
+  for (auto mov : MoveRelated(rs)){
+    if (activeMoves.find(mov) != activeMoves.end()) {
+      activeMoves.erase(mov);
+      PushVecSingleVal(worklistMoves, mov);
+    }
+  }
+  auto t = Adjacent(rs);
+  std::unordered_set<MOperand> tmp(t.begin(), t.end());
+  // EnableMove
+  for (auto node : AdjList[rs]) {
+    // Add Edge
+    AddEdge(node, rd);
+    DecrementDegree(node);
+  }
+  if (Degree[rd] >= GetRegNums(rs) &&
+      (freezeWorkList.find(rd) != freezeWorkList.end())) {
+    freezeWorkList.erase(rd);
+    spillWorkList.insert(rd);
+  }
+}
+
+//将某个变量 freeze 所关联的所有 move 指令标记为 frozen（不再尝试合并），并更新工作列表。
+void GraphColor::FreezeMoves(MOperand freeze) {
+  auto tmp = MoveRelated(freeze);
+  for (auto mov : tmp) {
+    // mov: dst<-src
+    MOperand dst = dynamic_cast<MOperand>(mov->GetDef());
+    MOperand src = dynamic_cast<MOperand>(mov->GetOperand(0));
+    MOperand value;
+    //找到除了freeze的另外一个操作数
+    if (GetAlias(src) == GetAlias(freeze)) {
+      value = GetAlias(dst);
+    } else {
+      value = GetAlias(src);
+    }
+    activeMoves.erase(mov);
+    frozenMoves.insert(mov);
+    if (MoveRelated(value).size() == 0) {
+      if ((value->GetType() == riscv_i32 &&Degree[value] < reglist.GetReglistInt().size()) ||(value->GetType() == riscv_i64 && Degree[value] < reglist.GetReglistInt().size()) || (value->GetType() == riscv_float32 && Degree[value] < reglist.GetReglistFloat().size()) || (value->GetType() == riscv_ptr && Degree[value] < reglist.GetReglistInt().size())) {
+        freezeWorkList.erase(value);
+        PushVecSingleVal(simplifyWorkList, value);
+      }
+    }
+  }
+}
+//将swl中的一个变量弹出准备分配颜色
+void GraphColor::simplify(){
+  auto val = simplifyWorkList.back();
+  simplifyWorkList.pop_back();
+  selectstack.push_back(val);
+
+  _DEBUG(std::cerr << "SelectStack Insert: " << val->GetName() << std::endl;)
+
+  auto adj = Adjacent(val);
+for (auto target : AdjList[val]) {
+  DecrementDegree(target);
+}
+//遍历 val 的邻居，对邻接节点执行 DecrementDegree（度数减 1，并更新其状态）
+}
+
+void GraphColor::coalesce(){
+  for(int i=0;i<worklistMoves.size();i++){
+    using MoveOperand = std::pair<MOperand, MOperand>;
+    MoveOperand m;
+    RISCVMIR *mv = worklistMoves[i];
+      // rd <- rs
+    MOperand rd = dynamic_cast<MOperand>(mv->GetDef()->ignoreLA());
+    MOperand rs = dynamic_cast<MOperand>(mv->GetOperand(0)->ignoreLA());
+    rd = GetAlias(rd);
+    rs = GetAlias(rs);
+    //如果某个寄存器之前已经合并,则返回其别名
+    //始终让 m.first 是 Precolored 的（如果有）。
+    if (Precolored.find(rs) != Precolored.end()) {
+      m = std::make_pair(rs, rd);
+    } else {
+      m = std::make_pair(rd, rs);
+    }
+    vec_pop(worklistMoves,i);//移除已经处理的move指令,避免重复处理
+    if(m.first==m.second){
+      coalescedMoves.push_back(mv);
+      AddWorkList(m.first);
+    }else if(Precolored.find(m.second) != Precolored.end() ||adjSet[m.first].find(m.second) != adjSet[m.first].end()){
+      //如果都已经预着色,或者在干涉图中相邻,那就不能合并
+      constrainedMoves.insert(mv);
+      AddWorkList(m.first);
+      AddWorkList(m.second);
+    }else if((Precolored.find(m.first) != Precolored.end() &&
+    GeorgeCheck(m.second, m.first, m.second->GetType())) ||
+   (Precolored.find(m.first) == Precolored.end() &&
+    BriggsCheck(m.second, m.first, m.first->GetType()))){
+      coalescedMoves.push_back(mv);
+      combine(m.first, m.second);
+      AddWorkList(m.first);
+    }else{
+      activeMoves.insert(mv);
+    }
+  }
+}
+
+void GraphColor::freeze() {
+  auto freeze = HeuristicFreeze();
+  freezeWorkList.erase(freeze);
+  PushVecSingleVal(simplifyWorkList, freeze);
+  //放弃对这个freeze节点合并的希望，将他看成是传送无关节点
+  FreezeMoves(freeze);
+}
+// Spill 的意义：
+// 溢出变量并不代表马上生成 spill 指令。
+
+// 它会正常参与图着色过程。
+
+// 若最终着色失败，才会真正生成 store/load 指令。
+void GraphColor::spill() {
+  auto spill = HeuristicSpill();
+  if (spill == nullptr)
+    return;
+  spillWorkList.erase(spill);
+  _DEBUG(std::cerr << "Choose To Spill element: " << spill->GetName()
+                   << std::endl;)
+  SpillStack.push_back(spill);
+  PushVecSingleVal(simplifyWorkList, spill);
+  FreezeMoves(spill);
+}
+
+//终于到了,分配实际物理寄存器!
+void GraphColor::AssignColors(){
+  MOperand select = selectstack.front();
+  RISCVType ty = select->GetType();
+  selectstack.erase(selectstack.begin());
+  std::unordered_set<MOperand> int_assist{reglist.GetReglistInt().begin(),reglist.GetReglistInt().end()};
+  std::unordered_set<MOperand> float_assist{reglist.GetReglistFloat().begin(), reglist.GetReglistFloat().end()};
+  //遍历所有的冲突点，查看他们分配的颜色，保证我们分配的颜色一定是不同的
+    for(auto adj:AdjList[select]){
+      if (coloredNode.find(GetAlias(adj)) != coloredNode.end() ||
+          Precolored.find(GetAlias(adj)) != Precolored.end()) {
+        if (color.find(dynamic_cast<MOperand>(GetAlias(adj))) == color.end())
+          assert(0);
+        if (ty == riscv_i32 || ty == riscv_ptr || ty == riscv_i64)
+          int_assist.erase(color[GetAlias(adj)]);
+        else if (ty == riscv_float32)
+          float_assist.erase(color[GetAlias(adj)]);
+      }
+    }
+    
 }
