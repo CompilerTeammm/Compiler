0c6b859ee3fb29f906a17769a2178026afc540b8
hu1 <3047853373@qq.com>
2025-05-11 21:32:44 +0800

    todo debug

--- IR/Opt/SSAPRE.cpp
+++ IR/Opt/SSAPRE.cpp
@@ -1,6 +1,20 @@
 #include "../../include/IR/Opt/SSAPRE.hpp"
-#include <unordered_map>
 
+std::set<BasicBlock*> ComputeInsertPoints(DominantTree* tree,const std::set<BasicBlock*>& blocksWithExpr){
+    IDFCalculator idfCalc(*tree);
+    std::set<BasicBlock*> defBlocks = blocksWithExpr;
+    idfCalc.setDefiningBlocks(defBlocks);
+
+    std::vector<BasicBlock*> IDFBlocks;
+    idfCalc.calculate(IDFBlocks);
+
+    return std::set<BasicBlock*>(IDFBlocks.begin(),IDFBlocks.end());
+}
+// Instruction* SSAPRE::InsertExprAt(BasicBlock* bb, Instruction::Op op, Value* lhs, Value* rhs) {
+//     auto* newInst = new BinaryInst(lhs,op, rhs);
+//     bb->InsertFront(newInst);
+//     return newInst;
+// }
 bool SSAPRE::BeginToChange(){
     for(auto& [key,occurList]:exprToOccurList){
         //找出所有使用该表达式的块
@@ -8,6 +22,26 @@ bool SSAPRE::BeginToChange(){
         for(auto* inst:occurList){
             blocksWithExpr.insert(inst->GetParent());
         }
+
+        //计算DF支配边界,找到插入点
+        std::set<BasicBlock*> insertPoints=ComputeInsertPoints(blocksWithExpr);
+
+        //在插入点插入表达式,生成新的SSA临时变量
+        assert(!occurList.empty());
+        auto* firstInst=dynamic_cast<BinaryInst*>(occurList[0]);
+        if(!firstInst) continue;
+        Operand lhs = firstInst->GetOperand(0);  // 第一个操作数
+        Operand rhs = firstInst->GetOperand(1);  // 第二个操作数
+        auto op = firstInst->GetOp(); //运算类型
+        //记录插入点及其对应的新定义值（为后续替换做准备）
+        std::unordered_map<BasicBlock*, Operand> insertPointToNewValue;
+        for(auto* bb:insertPoints){
+            auto newInst=new BinaryInst(lhs,op,rhs);
+            auto* insertPos=bb->GetFront();
+            insertPos->InsertBefore(newInst);
+            insertPointToNewValue[bb]=newInst->GetName();
+        }
+        //替换冗余表达式
         
     }
 }
--- include/IR/Opt/SSAPRE.hpp
+++ include/IR/Opt/SSAPRE.hpp
@@ -3,7 +3,8 @@
 #include "../Analysis/Dominant.hpp"
 #include "../../lib/CoreClass.hpp"
 #include "../../lib/CFG.hpp"
-
+#include "IDF.hpp"
+#include <unordered_map>
 
 class SSAPRE :public _PassBase<SSAPRE,Function>
 {
@@ -14,6 +15,8 @@ public:
     bool PartialRedundancyElimination(Function* func);
     //实际上只是检测是否有部分冗余，接下来交给BeginToChange
     bool BeginToChange();
+    std::set<BasicBlock*> ComputeInsertPoints(std::set<BasicBlock*>);
+    // Instruction* InsertExprAt(BasicBlock*,Instruction::Op,Value*,Value*);
     ~SSAPRE() = default;
 private:
     Function* func;
