2907a5601f1c8eb4d31b9b068f19745fa4428763
hu1 <3047853373@qq.com>
2025-05-20 22:04:57 +0800

    finish mergeBlocks

--- IR/Opt/SimplifyCFG.cpp
+++ IR/Opt/SimplifyCFG.cpp
@@ -31,7 +31,7 @@ bool SimplifyCFG::SimplifyCFGBasicBlock(BasicBlock* bb){
 }
 
 //删除不可达基本块(记得要把phi引用到的也进行处理)
-bool removeUnreachableBlocks(Function* func){
+bool SimplifyCFG::removeUnreachableBlocks(Function* func){
     std::unordered_set<BasicBlock*> reachable;//存储可达块
     std::stack<BasicBlock*> bbstack;
 
@@ -71,7 +71,7 @@ bool removeUnreachableBlocks(Function* func){
     return changed;
 }
 
-//合并空返回块
+//合并空返回块(no phi)
 bool SimplifyCFG::mergeEmptyReturnBlocks(Function* func){
     auto& BBs=func->GetBBs();
     std::vector<BasicBlock*> returnBlocks;
@@ -93,6 +93,58 @@ bool SimplifyCFG::mergeEmptyReturnBlocks(Function* func){
     //重定向其他返回块的前驱到commonRet,并清理phi
     for(size_t i=1;i<returnBlocks.size();++i){
         BasicBlock* redundant=returnBlocks[i];
-        for(auto* pred:)
+        for(auto* pred: redundant->GetPredBlocks()){
+            pred->ReplaceNextBlock(redundant,commonRet);//替换后继
+            commonRet->AddPredBlock(pred);//加入新前驱
+        }
+
+        //清理phi对redundant的引用,如果有的话
+        //我觉得按照我们phi函数生成的逻辑应该没有,先不加了
+        func->RemoveBBs(redundant);
     }
+    return true;
+}
+
+//合并基本块(no phi)
+bool SimplifyCFG::mergeBlocks(BasicBlock* bb){
+    //获取后继块
+    if(bb->GetNextBlocks().size()!=1){
+        return false;
+    }
+    auto succ=bb->GetNextBlocks()[0];
+    //后继不能是自身,避免死循环
+    if(succ==bb){
+        return false;
+    }
+    //判断succ是否只有bb一个前驱
+    if(succ->GetPredBlocks().size()!=1||succ->GetPredBlocks()[0]!=bb){
+        return false;
+    }
+
+    //ok,那满足条件,合并
+    //移除bb中的terminator指令(一般是br)
+    if(bb->Size()!=0 && bb->GetBack()->IsTerminateInst()){
+        bb->GetBack()->EraseFromManager();
+    }
+    while(succ->Size()!=0){
+        Instruction *inst=succ->GetFront();
+        inst->EraseFromManager();
+        bb->push_back(inst);
+    }
+    //更新CFG
+    //断开bb与succ
+    bb->RemoveNextBlock(succ);
+    succ->RemovePredBlock(bb);
+    //succ的后继接到bb上
+    for(auto succsucc:succ->GetNextBlocks()){
+        succsucc->RemovePredBlock(succ);
+        succsucc->AddPredBlock(bb);
+        bb->AddNextBlock(succsucc);
+    }
+    succ->EraseFromManager();
+    return true;
+}
+
+bool SimplifyCFG::simplifyBranch(BasicBlock* bb){
+    
 }
