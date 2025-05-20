a2655f764d9597c30a0011729e42e8bc4fcce73d
hu1 <3047853373@qq.com>
2025-05-20 15:56:31 +0800

    save

--- IR/Opt/SimplifyCFG.cpp
+++ IR/Opt/SimplifyCFG.cpp
@@ -29,6 +29,7 @@ bool SimplifyCFG::SimplifyCFGBasicBlock(BasicBlock* bb){
 
     return changed;
 }
+
 //删除不可达基本块(记得要把phi引用到的也进行处理)
 bool removeUnreachableBlocks(Function* func){
     std::unordered_set<BasicBlock*> reachable;//存储可达块
@@ -56,7 +57,7 @@ bool removeUnreachableBlocks(Function* func){
         BasicBlock* bb=it->get();
         if(reachable.count(bb)==0){
             for(auto pred:bb->GetPredBlocks()){
-                pred->RemoveSuccBlock(bb);//!need to do
+                pred->RemoveNextBlock(bb);
             }
             for(auto succ:bb->GetNextBlocks()){
                 succ->RemovePredBlock(bb);
@@ -68,4 +69,30 @@ bool removeUnreachableBlocks(Function* func){
         }
     }
     return changed;
+}
+
+//合并空返回块
+bool SimplifyCFG::mergeEmptyReturnBlocks(Function* func){
+    auto& BBs=func->GetBBs();
+    std::vector<BasicBlock*> returnBlocks;
+
+    //收集所有空ret块
+    for(auto& bbPtr:BBs){
+        BasicBlock* bb=bbPtr.get();
+        if(bb->Size()==1&&bb->front->id==Instruction::Op::Ret){
+            returnBlocks.push_back(bb);
+        }
+    }
+    //合并空ret块
+    if(returnBlocks.size()<=1){
+        return false;
+    }
+    //选定第一个作为公共返回块
+    BasicBlock* commonRet=returnBlocks.front();
+
+    //重定向其他返回块的前驱到commonRet,并清理phi
+    for(size_t i=1;i<returnBlocks.size();++i){
+        BasicBlock* redundant=returnBlocks[i];
+        for(auto* pred:)
+    }
 }
