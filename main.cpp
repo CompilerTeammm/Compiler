9126bdabf4907a335d0970c59b382f7cee3b68ec
bigSheep123 <1554869970@qq.com>
2025-07-22 02:08:51 +0800

    save

--- main.cpp
+++ main.cpp
@@ -10,45 +10,37 @@
 #include "Log/log.hpp"
 #include <filesystem>
 
-// #define OPT
+#define OPT
 //#define backend
 
 extern FILE *yyin;
 extern int optind, opterr, optopt;
 extern char *optargi;
 std::string asmoutput_path;
-void copyFile(const std::string &sourcePath,
-              const std::string &destinationPath)
-{
-   std::ifstream source(sourcePath, std::ios::binary);
-   std::ofstream destination(destinationPath, std::ios::binary);
-   destination << source.rdbuf();
+void copyFile(const std::string &sourcePath, const std::string &destinationPath) {
+  std::ifstream source(sourcePath, std::ios::binary);
+  std::ofstream destination(destinationPath, std::ios::binary);
+  destination << source.rdbuf();
 }
 
-int main(int argc, char **argv)
-{
-   std::string output_path = argv[1];
-   output_path += ".ll";
+int main(int argc, char **argv) {
+  std::string output_path = argv[1];
+  output_path += ".ll";
 
-   std::string filename = argv[1];
-   size_t lastSlashPos = filename.find_last_of("/\\") + 1;
-   filename = filename.substr(lastSlashPos);
+  std::string filename = argv[1];
+  size_t lastSlashPos = filename.find_last_of("/\\") + 1;
+  filename = filename.substr(lastSlashPos);
 
-   asmoutput_path = argv[1];
-   size_t lastPointPos = asmoutput_path.find_last_of(".");
-   asmoutput_path = asmoutput_path.substr(0, lastPointPos) + ".s";
+  asmoutput_path = argv[1];
+  size_t lastPointPos = asmoutput_path.find_last_of(".");
+  asmoutput_path = asmoutput_path.substr(0, lastPointPos) + ".s";
 
-
-   // copyFile("runtime.ll", output_path);
-   // clear the file 
-   std::ofstream ofs(output_path, std::ios::trunc); 
-   ofs.close();
-
-   freopen(output_path.c_str(), "a", stdout);
-   yyin = fopen(argv[1], "r");
-   yy::parser parse;
-   parse();
-   Singleton<CompUnit *>()->codegen();
+  copyFile("runtime.ll", output_path);
+  freopen(output_path.c_str(), "a", stdout);
+  yyin = fopen(argv[1], "r");
+  yy::parser parse;
+  parse();
+  Singleton<CompUnit *>()->codegen();
 
    // 中端 前端要是测试可以把这段代码注释掉即可
 #ifdef OPT
